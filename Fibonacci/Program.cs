814e60e89c13a445a4f1de8640af852860d8e5b4
DimitraK92 <79407886+DimitraK92@users.noreply.github.com>
2021-05-14 22:55:49 +0300

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] fibonacci = Fibonacci(50);          
+
+            foreach (double num in fibonacci)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+  
+        }
+
+        static double[] Fibonacci (int size)
+        {
+            double[] fibonacci = new double[size];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            int counter = 2;
+            while (counter < size)
+            {
+                for (int i = 1; i < counter; i++)
+                {                 
+                   fibonacci[counter] = fibonacci[counter - 2] + fibonacci[i];                    
+                }
+                counter++;
+
+            }
+            return fibonacci;
+        }
+    }
+}
